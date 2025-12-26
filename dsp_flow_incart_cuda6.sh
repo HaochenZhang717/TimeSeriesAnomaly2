a8a77e764bacd013cefce7dc0a5fe617a1b71982
Haochen Zhang <hz112@rice.edu>
2025-12-26 13:18:26 -0600

    1226

--- dsp_flow_incart_cuda6.sh
+++ dsp_flow_incart_cuda6.sh
@@ -8,86 +8,84 @@ ONE_CHANNEL=0
 FEAT_SIZE=12
 
 DATA_TYPE="ecg"
-WANDB_PROJECT="dsp_flow_incart"
+WANDB_PROJECT="dsp_flow_incart_3in1"
 
-VQVAE_CKPT="/root/tianyi/formal_experiment/incart_v_anomaly/dsp_flow/vqvae_save_path"
-PRETRAIN_CKPT="/root/tianyi/formal_experiment/incart_v_anomaly/dsp_flow/no_context_pretrain_ckpt"
-FINETUNE_CKPT="/root/tianyi/formal_experiment/incart_v_anomaly/dsp_flow/impute_finetune_ckpt_lr${LR}"
+VQVAE_CKPT="/root/tianyi/formal_experiment/incart_3in1/dsp_flow/vqvae_save_path"
+PRETRAIN_CKPT="/root/tianyi/formal_experiment/incart_3in1/dsp_flow/no_context_pretrain_ckpt"
+FINETUNE_CKPT="/root/tianyi/formal_experiment/incart_3in1/dsp_flow/impute_finetune_ckpt_lr${LR}"
 
 
-DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_incart/I08.npz"]'
-TEST_DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_incart/I08.npz"]'
-PRETRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I08npz/normal_450.jsonl"]'
-FINETUNE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I08npz/V_train.jsonl"]'
-FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I08npz/V_test.jsonl"]'
-ANOMALY_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I08npz/V_segments_train.jsonl"]'
-NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I08npz/normal_800.jsonl"]'
+DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_incart/I03.npz","./dataset_utils/ECG_datasets/raw_data_incart/I52.npz","./dataset_utils/ECG_datasets/raw_data_incart/I69.npz"]'
+TEST_DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_incart/I03.npz","./dataset_utils/ECG_datasets/raw_data_incart/I52.npz","./dataset_utils/ECG_datasets/raw_data_incart/I69.npz"]'
+PRETRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I03npz/normal_450.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I52npz/normal_450.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I69npz/normal_450.jsonl"]'
+FINETUNE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I03npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I52npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I69npz/V_train.jsonl"]'
+FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I03npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I52npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I69npz/V_test.jsonl"]'
+ANOMALY_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I03npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I52npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I69npz/V_segments_train.jsonl"]'
+NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I03npz/normal_800.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I52npz/normal_800.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I69npz/normal_800.jsonl"]'
 
 #VQVAE Train Parameters
-VQVAE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I08npz/normal_800.jsonl"]'
+VQVAE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I03npz/normal_800.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I52npz/normal_800.jsonl","./dataset_utils/ECG_datasets/indices_incart/slide_windows_I69npz/normal_800.jsonl"]'
 CODE_DIM=8
 CODE_LEN=4
 NUM_CODES=500
 
 
-
-
-python dsp_flow.py \
-  --what_to_do "no_context_sample" \
-  \
-  --seq_len ${LEN_WHOLE} \
-  --data_type ${DATA_TYPE} \
-  --feature_size ${FEAT_SIZE} \
-  --one_channel ${ONE_CHANNEL} \
-  \
-  --n_layer_enc 4 \
-  --n_layer_dec 4 \
-  --d_model 64 \
-  --n_heads 4 \
-  \
-  --raw_data_paths_train ${DATA_PATHS} \
-  --raw_data_paths_test ${TEST_DATA_PATHS} \
-  --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
-  --indices_paths_test "[]" \
-  --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
-  --min_infill_length ${MIN_LEN_ANOMALY} \
-  --max_infill_length ${MAX_LEN_ANOMALY} \
-  \
-  --lr 1e-4 \
-  --batch_size 64 \
-  --max_epochs 2000 \
-  --grad_clip_norm 1.0 \
-  --grad_accum_steps 1 \
-  --early_stop "true" \
-  --patience 50 \
-  \
-  --wandb_project "none" \
-  --wandb_run "none" \
-  \
-  --ckpt_dir ${PRETRAIN_CKPT} \
-  --pretrained_ckpt "none" \
-  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
-  \
-  --generated_path "" \
-  \
-  --gpu_id ${GPU_ID}
-
-
-
-
-#python mini_runnable_vqvae.py \
-#  --max_seq_len ${MAX_LEN_ANOMALY} \
-#  --min_seq_len ${MIN_LEN_ANOMALY} \
-#  --data_paths ${DATA_PATHS} \
-#  --indices_paths ${VQVAE_TRAIN_INDICES_PATHS}\
+#python dsp_flow.py \
+#  --what_to_do "no_context_sample" \
+#  \
+#  --seq_len ${LEN_WHOLE} \
 #  --data_type ${DATA_TYPE} \
-#  --gpu_id ${GPU_ID} \
-#  --save_dir ${VQVAE_CKPT} \
-#  --code_dim ${CODE_DIM} \
-#  --code_len ${CODE_LEN} \
-#  --num_codes ${NUM_CODES} \
+#  --feature_size ${FEAT_SIZE} \
 #  --one_channel ${ONE_CHANNEL} \
-#  --feat_size ${FEAT_SIZE}
+#  \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  \
+#  --raw_data_paths_train ${DATA_PATHS} \
+#  --raw_data_paths_test ${TEST_DATA_PATHS} \
+#  --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
+#  --indices_paths_test "[]" \
+#  --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
+#  --min_infill_length ${MIN_LEN_ANOMALY} \
+#  --max_infill_length ${MAX_LEN_ANOMALY} \
+#  \
+#  --lr 1e-4 \
+#  --batch_size 64 \
+#  --max_epochs 2000 \
+#  --grad_clip_norm 1.0 \
+#  --grad_accum_steps 1 \
+#  --early_stop "true" \
+#  --patience 50 \
+#  \
+#  --wandb_project "none" \
+#  --wandb_run "none" \
+#  \
+#  --ckpt_dir ${PRETRAIN_CKPT} \
+#  --pretrained_ckpt "none" \
+#  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+#  \
+#  --generated_path "" \
+#  \
+#  --gpu_id ${GPU_ID}
+
+
+
+
+python mini_runnable_vqvae.py \
+  --max_seq_len ${MAX_LEN_ANOMALY} \
+  --min_seq_len ${MIN_LEN_ANOMALY} \
+  --data_paths ${DATA_PATHS} \
+  --indices_paths ${VQVAE_TRAIN_INDICES_PATHS}\
+  --data_type ${DATA_TYPE} \
+  --gpu_id ${GPU_ID} \
+  --save_dir ${VQVAE_CKPT} \
+  --code_dim ${CODE_DIM} \
+  --code_len ${CODE_LEN} \
+  --num_codes ${NUM_CODES} \
+  --one_channel ${ONE_CHANNEL} \
+  --feat_size ${FEAT_SIZE}
 
 
 #python dsp_flow.py \
