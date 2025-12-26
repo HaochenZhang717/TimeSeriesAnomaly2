2c8bbfb2debecdf1018736c6bb8e52fb1d0ecfbc
Haochen Zhang <hz112@rice.edu>
2025-12-26 15:46:39 -0600

    1226

--- dsp_flow_svdb.sh
+++ dsp_flow_svdb.sh
@@ -24,7 +24,7 @@ ANOMALY_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_svdb/slide_wi
 NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/normal_800.jsonl"]'
 
 #VQVAE Train Parameters
-VQVAE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/normal_800.jsonl"]'
+VQVAE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/normal_360.jsonl"]'
 CODE_DIM=8
 CODE_LEN=4
 NUM_CODES=500
