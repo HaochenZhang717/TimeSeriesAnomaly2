d84ec20f3ca04c617be6d2ed0230c1430bb2530b
Haochen Zhang <hz112@rice.edu>
2025-12-29 13:03:38 -0600

    1229

--- dsp_no_code/dsp_flow_svdb_no_code.sh
+++ dsp_no_code/dsp_flow_svdb_no_code.sh
@@ -27,45 +27,45 @@ ANOMALY_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_svdb/slide_wi
 NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/normal_800.jsonl"]'
 
 
-python dsp_flow.py \
-  --what_to_do "no_context_no_code_pretrain" \
-  \
-  --seq_len ${LEN_WHOLE} \
-  --data_type ${DATA_TYPE} \
-  --feature_size ${FEAT_SIZE} \
-  --one_channel ${ONE_CHANNEL} \
-  \
-  --n_layer_enc 4 \
-  --n_layer_dec 4 \
-  --d_model 64 \
-  --n_heads 4 \
-  \
-  --raw_data_paths_train ${DATA_PATHS} \
-  --raw_data_paths_test ${TEST_DATA_PATHS} \
-  --indices_paths_train ${PRETRAIN_INDICES_PATHS} \
-  --indices_paths_test "[]" \
-  --indices_paths_anomaly_for_sample "[]" \
-  --min_infill_length ${MIN_LEN_ANOMALY} \
-  --max_infill_length ${MAX_LEN_ANOMALY} \
-  \
-  --lr 1e-4 \
-  --batch_size 64 \
-  --max_epochs 250 \
-  --grad_clip_norm 1.0 \
-  --grad_accum_steps 1 \
-  --early_stop "true" \
-  --patience 50 \
-  \
-  --wandb_project ${WANDB_PROJECT} \
-  --wandb_run "no_context_no_code_pretrain" \
-  \
-  --ckpt_dir ${PRETRAIN_CKPT} \
-  --pretrained_ckpt "none" \
-  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt"\
-  \
-  --generated_path "none" \
-  \
-  --gpu_id ${GPU_ID}
+#python dsp_flow.py \
+#  --what_to_do "no_context_no_code_pretrain" \
+#  \
+#  --seq_len ${LEN_WHOLE} \
+#  --data_type ${DATA_TYPE} \
+#  --feature_size ${FEAT_SIZE} \
+#  --one_channel ${ONE_CHANNEL} \
+#  \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  \
+#  --raw_data_paths_train ${DATA_PATHS} \
+#  --raw_data_paths_test ${TEST_DATA_PATHS} \
+#  --indices_paths_train ${PRETRAIN_INDICES_PATHS} \
+#  --indices_paths_test "[]" \
+#  --indices_paths_anomaly_for_sample "[]" \
+#  --min_infill_length ${MIN_LEN_ANOMALY} \
+#  --max_infill_length ${MAX_LEN_ANOMALY} \
+#  \
+#  --lr 1e-4 \
+#  --batch_size 64 \
+#  --max_epochs 250 \
+#  --grad_clip_norm 1.0 \
+#  --grad_accum_steps 1 \
+#  --early_stop "true" \
+#  --patience 50 \
+#  \
+#  --wandb_project ${WANDB_PROJECT} \
+#  --wandb_run "no_context_no_code_pretrain" \
+#  \
+#  --ckpt_dir ${PRETRAIN_CKPT} \
+#  --pretrained_ckpt "none" \
+#  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt"\
+#  \
+#  --generated_path "none" \
+#  \
+#  --gpu_id ${GPU_ID}
 
 
 
@@ -96,7 +96,7 @@ python dsp_flow.py \
   --grad_clip_norm 1.0 \
   --grad_accum_steps 1 \
   --early_stop "true" \
-  --patience 50 \
+  --patience 500 \
   \
   --wandb_project ${WANDB_PROJECT} \
   --wandb_run "no_code_impute_finetune_lr${LR}" \
@@ -184,7 +184,7 @@ python dsp_flow.py \
   --wandb_project "none" \
   --wandb_run "none" \
   \
-  --ckpt_dir "${VQVAE_CKPT}" \
+  --ckpt_dir "${FINETUNE_CKPT}" \
   --pretrained_ckpt "none" \
   --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
   \
