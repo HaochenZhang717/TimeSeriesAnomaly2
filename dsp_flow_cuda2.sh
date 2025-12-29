d5916b398af199417e685d885bbee64a48ce8cfa
Haochen Zhang <hz112@rice.edu>
2025-12-29 12:13:08 -0600

    1229

--- dsp_flow_cuda2.sh
+++ dsp_flow_cuda2.sh
@@ -90,45 +90,45 @@ NUM_CODES=500
 
 
 
-python dsp_flow.py \
-  --what_to_do "imputation_finetune" \
-  \
-  --seq_len ${LEN_WHOLE} \
-  --data_type ${DATA_TYPE} \
-  --feature_size ${FEAT_SIZE} \
-  --one_channel ${ONE_CHANNEL} \
-  \
-  --n_layer_enc 4 \
-  --n_layer_dec 4 \
-  --d_model 64 \
-  --n_heads 4 \
-  \
-  --raw_data_paths_train ${DATA_PATHS} \
-  --raw_data_paths_test ${TEST_DATA_PATHS} \
-  --indices_paths_train ${FINETUNE_TRAIN_INDICES_PATHS} \
-  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
-  --indices_paths_anomaly_for_sample "[]" \
-  --min_infill_length ${MIN_LEN_ANOMALY} \
-  --max_infill_length ${MAX_LEN_ANOMALY} \
-  \
-  --lr ${LR} \
-  --batch_size 64 \
-  --max_epochs 500 \
-  --grad_clip_norm 1.0 \
-  --grad_accum_steps 1 \
-  --early_stop "true" \
-  --patience 50 \
-  \
-  --wandb_project ${WANDB_PROJECT} \
-  --wandb_run "impute_finetune_lr${LR}" \
-  \
-  --ckpt_dir ${FINETUNE_CKPT} \
-  --pretrained_ckpt ${PRETRAIN_CKPT} \
-  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
-  \
-  --generated_path "none" \
-  \
-  --gpu_id ${GPU_ID}
+#python dsp_flow.py \
+#  --what_to_do "imputation_finetune" \
+#  \
+#  --seq_len ${LEN_WHOLE} \
+#  --data_type ${DATA_TYPE} \
+#  --feature_size ${FEAT_SIZE} \
+#  --one_channel ${ONE_CHANNEL} \
+#  \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  \
+#  --raw_data_paths_train ${DATA_PATHS} \
+#  --raw_data_paths_test ${TEST_DATA_PATHS} \
+#  --indices_paths_train ${FINETUNE_TRAIN_INDICES_PATHS} \
+#  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+#  --indices_paths_anomaly_for_sample "[]" \
+#  --min_infill_length ${MIN_LEN_ANOMALY} \
+#  --max_infill_length ${MAX_LEN_ANOMALY} \
+#  \
+#  --lr ${LR} \
+#  --batch_size 64 \
+#  --max_epochs 500 \
+#  --grad_clip_norm 1.0 \
+#  --grad_accum_steps 1 \
+#  --early_stop "true" \
+#  --patience 50 \
+#  \
+#  --wandb_project ${WANDB_PROJECT} \
+#  --wandb_run "impute_finetune_lr${LR}" \
+#  \
+#  --ckpt_dir ${FINETUNE_CKPT} \
+#  --pretrained_ckpt ${PRETRAIN_CKPT} \
+#  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+#  \
+#  --generated_path "none" \
+#  \
+#  --gpu_id ${GPU_ID}
 
 
 python dsp_flow.py \
@@ -163,7 +163,7 @@ python dsp_flow.py \
   --wandb_project "none" \
   --wandb_run "none" \
   \
-  --ckpt_dir "${FINETUNE_CKPT}_123" \
+  --ckpt_dir "${FINETUNE_CKPT}" \
   --pretrained_ckpt "none" \
   --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
   \
