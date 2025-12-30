ccc7e542402e045c4be122eb8fb6c55a7f1f482e
Haochen Zhang <hz112@rice.edu>
2025-12-30 11:54:42 -0600

    1230

---
+++ LSTM_evaluate/lstm_dsp_mitdb.sh
@@ -0,0 +1,24 @@
+export CUDA_VISIBLE_DEVICES=7
+
+cd ..
+
+DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data/106.npz"]'
+FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V_test.jsonl"]'
+FINETUNE_CKPT="/root/tianyi/formal_experiment/mitdb_new_vqvae/dsp_flow/impute_finetune_ckpt_lr${LR}"
+
+MAX_LEN_ANOMALY=800
+LEN_WHOLE=1000
+
+
+python run_lstm_evaluate.py \
+    --seq_len ${LEN_WHOLE} \
+    --feature_size 1 \
+    --one_channel 1 \
+    --raw_data_paths ${DATA_PATHS} \
+    --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    --ckpt_dir "${FINETUNE_CKPT}" \
+    --generated_path "${FINETUNE_CKPT}/posterior_impute_samples.pth" \
+    --gpu_id 0
+
+cd ./LSTM_evaluate
