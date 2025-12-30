6de715e7d8f9d8d087fc00446ff2f91cf9951613
Haochen Zhang <hz112@rice.edu>
2025-12-30 00:38:11 -0600

    1230

---
+++ Moment_evaluate/tune_for_classification_timevae_svdb.sh
@@ -0,0 +1,18 @@
+export CUDA_VISIBLE_DEVICES=6
+model_sizes=("large" "base" "small")
+
+for model_size in "${model_sizes[@]}"; do
+  echo "Running classification with model size: ${model_size}"
+
+  python classification.py \
+      --mode "linear_probing" \
+      --output_path "/root/tianyi/moment_eval/svdb/timevae/${model_size}" \
+      --train_data_path "/root/tianyi/formal_experiment/svdb_one_channel/TimeVAE/no_code_impute_from_scratch_ckpt_lr1e-4/no_code_impute_samples.pth"\
+      --test_data_path "../dataset_utils/ECG_datasets/test_data/svdb859_test_data.pt"\
+      --key_signal "all_samples" \
+      --key_label "all_labels" \
+      --model_name ${model_size} \
+      --one_channel "true"
+
+
+done
