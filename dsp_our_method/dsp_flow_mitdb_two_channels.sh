5b4ef6393e6f58d41a5961b67cc6ae8566c7dc9b
Haochen Zhang <hz112@rice.edu>
2026-01-01 23:10:09 -0600

    0101

--- dsp_our_method/dsp_flow_mitdb_two_channels.sh
+++ dsp_our_method/dsp_flow_mitdb_two_channels.sh
@@ -65,20 +65,20 @@ for i in ${!GPU_IDS[@]}; do
 
   CUDA_VISIBLE_DEVICES=${GPU} \
   python mini_runnable_vqvae.py \
-    --wandb_project ${WANDB_PROJECT} \
-    --wandb_project "VQVAE-K${NUM_CODES}" \
-    --max_seq_len ${MAX_LEN_ANOMALY} \
-    --min_seq_len ${MIN_LEN_ANOMALY} \
-    --data_paths ${DATA_PATHS} \
-    --indices_paths ${VQVAE_TRAIN_INDICES_PATHS} \
-    --data_type ${DATA_TYPE} \
-    --gpu_id 0 \
-    --save_dir ${VQVAE_CKPT}/K${NUM_CODES} \
-    --code_dim ${CODE_DIM} \
-    --code_len ${CODE_LEN} \
-    --num_codes ${NUM_CODES} \
-    --one_channel ${ONE_CHANNEL} \
-    --feat_size ${FEAT_SIZE} \
+  --wandb_project ${WANDB_PROJECT} \
+  --wandb_project "VQVAE-K${NUM_CODES}" \
+  --max_seq_len ${MAX_LEN_ANOMALY} \
+  --min_seq_len ${MIN_LEN_ANOMALY} \
+  --data_paths ${DATA_PATHS} \
+  --indices_paths ${VQVAE_TRAIN_INDICES_PATHS}\
+  --data_type ${DATA_TYPE} \
+  --gpu_id 0 \
+  --save_dir ${VQVAE_CKPT} \
+  --code_dim ${CODE_DIM} \
+  --code_len ${CODE_LEN} \
+  --num_codes ${NUM_CODES} \
+  --one_channel ${ONE_CHANNEL} \
+  --feat_size ${FEAT_SIZE}
     &   # 👈 后台运行
 done
 
