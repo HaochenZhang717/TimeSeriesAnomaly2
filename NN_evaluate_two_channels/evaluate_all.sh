e2d548ac379ebf25aed0b74b537e377de63c55ec
Haochen Zhang <hz112@rice.edu>
2026-01-03 19:05:50 -0600

    0103

--- NN_evaluate_two_channels/evaluate_all.sh
+++ NN_evaluate_two_channels/evaluate_all.sh
@@ -1,20 +1,20 @@
-CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_mitdb.sh 200 &
-CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_mitdb.sh 300 &
-CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_mitdb.sh 400 &
-CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_mitdb.sh 500 &
+#CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_mitdb.sh 200 &
+#CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_mitdb.sh 300 &
+#CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_mitdb.sh 400 &
+#CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_mitdb.sh 500 &
 
-#CUDA_VISIBLE_DEVICES=4 bash nn_eval_dsp_qtdb.sh 200 &
-#CUDA_VISIBLE_DEVICES=5 bash nn_eval_dsp_qtdb.sh 300 &
-#CUDA_VISIBLE_DEVICES=6 bash nn_eval_dsp_qtdb.sh 400 &
-#CUDA_VISIBLE_DEVICES=7 bash nn_eval_dsp_qtdb.sh 500 &
+CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_qtdb.sh 200 &
+CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_qtdb.sh 300 &
+CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_qtdb.sh 400 &
+CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_qtdb.sh 500 &
 
-CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_svdb.sh 200 &
-CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_svdb.sh 300 &
-CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_svdb.sh 400 &
-CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_svdb.sh 500 &
+#CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_svdb.sh 200 &
+#CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_svdb.sh 300 &
+#CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_svdb.sh 400 &
+#CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_svdb.sh 500 &
 
-#CUDA_VISIBLE_DEVICES=4 bash nn_eval_dsp_no_code_svdb.sh &
-#CUDA_VISIBLE_DEVICES=5 bash nn_eval_dsp_no_code_mitdb.sh &
-#CUDA_VISIBLE_DEVICES=6 bash nn_eval_dsp_no_code_qtdb.sh &
+CUDA_VISIBLE_DEVICES=4 bash nn_eval_dsp_no_code_svdb.sh &
+CUDA_VISIBLE_DEVICES=5 bash nn_eval_dsp_no_code_mitdb.sh &
+CUDA_VISIBLE_DEVICES=6 bash nn_eval_dsp_no_code_qtdb.sh &
 
 
