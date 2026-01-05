865a28c29419c47b4cf9394012f934ea3504afbe
Haochen Zhang <hz112@rice.edu>
2026-01-05 14:53:08 -0600

    0105

--- run_general_ts_gen_baseline.sh
+++ run_general_ts_gen_baseline.sh
@@ -1,7 +1,7 @@
 
 
 
-CUDA_VISIBLE_DEVICES=0 bash timevae_all/cgats_mitdb.sh &
+CUDA_VISIBLE_DEVICES=0 bash timevae_all/cgats_mitdb.sh
 
 #CUDA_VISIBLE_DEVICES=0 bash timevae_all/timevae_mitdb.sh &
 #CUDA_VISIBLE_DEVICES=1 bash timevae_all/timevae_qt.sh &
