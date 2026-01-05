351a134caa91803b16bf75e234c057e576c51666
Haochen Zhang <hz112@rice.edu>
2026-01-04 18:47:53 -0600

    0104

--- cgats_baseline/cgats_svdb.sh
+++ cgats_baseline/cgats_svdb.sh
@@ -15,34 +15,34 @@ FINETUNE_TEST_INDICES_PATHS='["/root/tianyi/TimeSeriesAnomaly2/dataset_utils/ECG
 
 WANDB_PROJECT="CGATS-SVDB"
 
-python CGATSPretrainPipeline.py \
-  --seq_len ${LEN_WHOLE} \
-  --feature_size ${FEATURE_SIZE} \
-  --one_channel ${ONE_CHANNEL} \
-  \
-  --latent_dim 64 \
-  --trend_poly 3 \
-  --kl_wt 1e-3 \
-  --hidden_layer_sizes "[50,100,200]" \
-  --custom_seas "[[10,80],[20,40],[40,20],[80,10]]" \
-  \
-  --max_anomaly_length ${MAX_LEN_ANOMALY} \
-  --min_anomaly_length ${MIN_LEN_ANOMALY} \
-  --raw_data_paths_train ${RAW_DATA_PATHS} \
-  --indices_paths_train ${PRETRAIN_INDICES_PATHS_TRAIN} \
-  \
-  --lr 1e-5 \
-  --batch_size 128 \
-  --epochs 1 \
-  --grad_clip_norm 1.0 \
-  --early_stop "true" \
-  --patience 100 \
-  \
-  --wandb_project ${WANDB_PROJECT} \
-  --wandb_run pretrain \
-  \
-  --ckpt_dir ${PRETRAIN_CKPT_DIR} \
-  --gpu_id 0
+#python CGATSPretrainPipeline.py \
+#  --seq_len ${LEN_WHOLE} \
+#  --feature_size ${FEATURE_SIZE} \
+#  --one_channel ${ONE_CHANNEL} \
+#  \
+#  --latent_dim 64 \
+#  --trend_poly 3 \
+#  --kl_wt 1e-3 \
+#  --hidden_layer_sizes "[50,100,200]" \
+#  --custom_seas "[[10,80],[20,40],[40,20],[80,10]]" \
+#  \
+#  --max_anomaly_length ${MAX_LEN_ANOMALY} \
+#  --min_anomaly_length ${MIN_LEN_ANOMALY} \
+#  --raw_data_paths_train ${RAW_DATA_PATHS} \
+#  --indices_paths_train ${PRETRAIN_INDICES_PATHS_TRAIN} \
+#  \
+#  --lr 1e-5 \
+#  --batch_size 128 \
+#  --epochs 1 \
+#  --grad_clip_norm 1.0 \
+#  --early_stop "true" \
+#  --patience 100 \
+#  \
+#  --wandb_project ${WANDB_PROJECT} \
+#  --wandb_run pretrain \
+#  \
+#  --ckpt_dir ${PRETRAIN_CKPT_DIR} \
+#  --gpu_id 0
 
 
 
