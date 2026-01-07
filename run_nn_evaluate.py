9f827bf5095a4e980de38cf5c5fa24ad84ad6b0c
Haochen Zhang <hz112@rice.edu>
2026-01-06 20:00:58 -0600

    0106

--- run_nn_evaluate.py
+++ run_nn_evaluate.py
@@ -285,7 +285,7 @@ def run_TCN_evaluate(args, real_data, real_labels, gen_data, gen_labels, device)
     f1s = []
     normal_accuracies = []
     anomaly_accuracies = []
-    for _ in range(3):
+    for _ in range(1):
         random_indices = torch.randperm(len(gen_data))[:10000]
         sampled_gen_data = gen_data[random_indices]
         sampled_gen_labels = gen_labels[random_indices]
@@ -489,8 +489,8 @@ def main():
 
 
     # run_robustTAD_evaluate(args, real_data, real_labels, gen_data, gen_labels, device)
-    # run_TCN_evaluate(args, real_data, real_labels, gen_data, gen_labels, device)
-    run_GPT4TS_evaluate(args, real_data, real_labels, gen_data, gen_labels, device)
+    run_TCN_evaluate(args, real_data, real_labels, gen_data, gen_labels, device)
+    # run_GPT4TS_evaluate(args, real_data, real_labels, gen_data, gen_labels, device)
     # run_moment_evaluate(
     #     real_data, real_labels, gen_data, gen_labels,
     #     model_name="large", one_channel=args.one_channel,
