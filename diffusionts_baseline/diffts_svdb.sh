6bcb4ae3c43da75e54e75f95bd236a096abc7a9b
Haochen Zhang <hz112@rice.edu>
2026-01-06 19:41:40 -0600

    0106

--- diffusionts_baseline/diffts_svdb.sh
+++ diffusionts_baseline/diffts_svdb.sh
@@ -5,8 +5,8 @@ LEN_WHOLE=800
 MAX_LEN_ANOMALY=450
 MIN_LEN_ANOMALY=80
 
-ONE_CHANNEL=1
-FEAT_SIZE=1
+ONE_CHANNEL=0
+FEAT_SIZE=2
 
 DATA_TYPE="ecg"
 WANDB_PROJECT="diffusion_ts_svdb"
@@ -25,43 +25,43 @@ EVENT_LABELS_PATHS='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_85
 
 
 
-#python diffusion_ts.py \
-#  --what_to_do "no_code_imputation_train" \
-#  \
-#  --seq_len ${LEN_WHOLE} \
-#  --data_type ${DATA_TYPE} \
-#  --feature_size ${FEAT_SIZE} \
-#  --one_channel ${ONE_CHANNEL} \
-#  \
-#  --n_layer_enc 4 \
-#  --n_layer_dec 4 \
-#  --d_model 64 \
-#  --n_heads 4 \
-#  \
-#  --raw_data_paths_train ${DATA_PATHS} \
-#  --raw_data_paths_test ${TEST_DATA_PATHS} \
-#  --indices_paths_train ${FINETUNE_TRAIN_INDICES_PATHS} \
-#  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
-#  --indices_paths_anomaly_for_sample "[]" \
-#  --min_infill_length ${MIN_LEN_ANOMALY} \
-#  --max_infill_length ${MAX_LEN_ANOMALY} \
-#  \
-#  --lr ${LR} \
-#  --batch_size 64 \
-#  --max_epochs 2000 \
-#  --grad_clip_norm 1.0 \
-#  --grad_accum_steps 1 \
-#  --early_stop "true" \
-#  --patience 500 \
-#  \
-#  --wandb_project ${WANDB_PROJECT} \
-#  --wandb_run "impute_lr${LR}" \
-#  \
-#  --ckpt_dir ${TRAIN_CKPT} \
-#  \
-#  --generated_path "none" \
-#  \
-#  --gpu_id 0
+python diffusion_ts.py \
+  --what_to_do "no_code_imputation_train" \
+  \
+  --seq_len ${LEN_WHOLE} \
+  --data_type ${DATA_TYPE} \
+  --feature_size ${FEAT_SIZE} \
+  --one_channel ${ONE_CHANNEL} \
+  \
+  --n_layer_enc 4 \
+  --n_layer_dec 4 \
+  --d_model 64 \
+  --n_heads 4 \
+  \
+  --raw_data_paths_train ${DATA_PATHS} \
+  --raw_data_paths_test ${TEST_DATA_PATHS} \
+  --indices_paths_train ${FINETUNE_TRAIN_INDICES_PATHS} \
+  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+  --indices_paths_anomaly_for_sample "[]" \
+  --min_infill_length ${MIN_LEN_ANOMALY} \
+  --max_infill_length ${MAX_LEN_ANOMALY} \
+  \
+  --lr ${LR} \
+  --batch_size 64 \
+  --max_epochs 2000 \
+  --grad_clip_norm 1.0 \
+  --grad_accum_steps 1 \
+  --early_stop "true" \
+  --patience 500 \
+  \
+  --wandb_project ${WANDB_PROJECT} \
+  --wandb_run "impute_lr${LR}" \
+  \
+  --ckpt_dir ${TRAIN_CKPT} \
+  \
+  --generated_path "none" \
+  \
+  --gpu_id 0
 
 
 python diffusion_ts.py \
