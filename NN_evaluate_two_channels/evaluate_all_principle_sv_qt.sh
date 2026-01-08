a4050405a1a72e1566e21e2daae7e0acda760128
Haochen Zhang <hz112@rice.edu>
2026-01-08 04:47:04 -0600

    0108

--- NN_evaluate_two_channels/evaluate_all_principle_sv_qt.sh
+++ NN_evaluate_two_channels/evaluate_all_principle_sv_qt.sh
@@ -1,13 +1,13 @@
 
-CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_no_code_qtdb.sh &
-CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_no_code_svdb.sh &
+#CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_no_code_qtdb.sh &
+#CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_no_code_svdb.sh &
 
-CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_qtdb.sh 500 &
-CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_svdb.sh 500 &
+#CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_qtdb.sh 500 &
+#CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_svdb.sh 500 &
 
-#CUDA_VISIBLE_DEVICES=4 bash nn_eval_diffts_qtdb.sh &
-#CUDA_VISIBLE_DEVICES=5 bash nn_eval_diffts_svdb.sh &
+CUDA_VISIBLE_DEVICES=4 bash nn_eval_diffts_qtdb.sh &
+CUDA_VISIBLE_DEVICES=5 bash nn_eval_diffts_svdb.sh &
 
-CUDA_VISIBLE_DEVICES=4 bash nn_eval_flowts_qtdb.sh &
-CUDA_VISIBLE_DEVICES=5 bash nn_eval_flowts_svdb.sh &
+#CUDA_VISIBLE_DEVICES=4 bash nn_eval_flowts_qtdb.sh &
+#CUDA_VISIBLE_DEVICES=5 bash nn_eval_flowts_svdb.sh &
 
