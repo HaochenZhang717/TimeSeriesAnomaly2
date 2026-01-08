862716c2f8b4372ac37196cbce6842b62ff37a91
Haochen Zhang <hz112@rice.edu>
2026-01-07 18:08:02 -0600

    0107

--- NN_evaluate_two_channels/evaluate_all.sh
+++ NN_evaluate_two_channels/evaluate_all.sh
@@ -1,9 +1,9 @@
 
-CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_no_code_mitdb.sh &
+#CUDA_VISIBLE_DEVICES=0 bash nn_eval_dsp_no_code_mitdb.sh &
 CUDA_VISIBLE_DEVICES=1 bash nn_eval_dsp_no_code_qtdb.sh &
 CUDA_VISIBLE_DEVICES=2 bash nn_eval_dsp_no_code_svdb.sh &
 
-CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_mitdb.sh 500 &
+#CUDA_VISIBLE_DEVICES=3 bash nn_eval_dsp_mitdb.sh 500 &
 CUDA_VISIBLE_DEVICES=4 bash nn_eval_dsp_qtdb.sh 500 &
 CUDA_VISIBLE_DEVICES=5 bash nn_eval_dsp_svdb.sh 500 &
 
