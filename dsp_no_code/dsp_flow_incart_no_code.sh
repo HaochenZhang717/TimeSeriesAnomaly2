284476a0f3f04db12872b885c2fc856683cde384
Haochen Zhang <hz112@rice.edu>
2026-01-09 09:35:50 -0600

    0109

--- dsp_no_code/dsp_flow_incart_no_code.sh
+++ dsp_no_code/dsp_flow_incart_no_code.sh
@@ -28,8 +28,51 @@ NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_incart/slide_w
 EVENT_LABELS_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I30npz/event_label.npy"]'
 
 
+#python dsp_flow.py \
+#  --what_to_do "no_context_no_code_pretrain" \
+#  \
+#  --seq_len ${LEN_WHOLE} \
+#  --data_type ${DATA_TYPE} \
+#  --feature_size ${FEAT_SIZE} \
+#  --one_channel ${ONE_CHANNEL} \
+#  --num_codes 500 \
+#  \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  \
+#  --raw_data_paths_train ${DATA_PATHS} \
+#  --raw_data_paths_test ${TEST_DATA_PATHS} \
+#  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+#  --indices_paths_train ${PRETRAIN_INDICES_PATHS} \
+#  --indices_paths_test "[]" \
+#  --indices_paths_anomaly_for_sample "[]" \
+#  --min_infill_length ${MIN_LEN_ANOMALY} \
+#  --max_infill_length ${MAX_LEN_ANOMALY} \
+#  \
+#  --lr 1e-4 \
+#  --batch_size 64 \
+#  --max_epochs 100 \
+#  --grad_clip_norm 1.0 \
+#  --grad_accum_steps 1 \
+#  --early_stop "true" \
+#  --patience 50 \
+#  \
+#  --wandb_project ${WANDB_PROJECT} \
+#  --wandb_run "no_context_no_code_pretrain" \
+#  \
+#  --ckpt_dir ${PRETRAIN_CKPT} \
+#  --pretrained_ckpt "none" \
+#  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt"\
+#  \
+#  --generated_path "none" \
+#  \
+#  --gpu_id 0
+
+
 python dsp_flow.py \
-  --what_to_do "no_context_no_code_pretrain" \
+  --what_to_do "no_code_imputation_finetune" \
   \
   --seq_len ${LEN_WHOLE} \
   --data_type ${DATA_TYPE} \
@@ -44,31 +87,32 @@ python dsp_flow.py \
   \
   --raw_data_paths_train ${DATA_PATHS} \
   --raw_data_paths_test ${TEST_DATA_PATHS} \
-  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
-  --indices_paths_train ${PRETRAIN_INDICES_PATHS} \
-  --indices_paths_test "[]" \
+  --indices_paths_train ${FINETUNE_TRAIN_INDICES_PATHS} \
+  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
   --indices_paths_anomaly_for_sample "[]" \
   --min_infill_length ${MIN_LEN_ANOMALY} \
   --max_infill_length ${MAX_LEN_ANOMALY} \
   \
-  --lr 1e-4 \
+  --lr ${LR} \
   --batch_size 64 \
-  --max_epochs 100 \
+  --max_epochs 500 \
   --grad_clip_norm 1.0 \
   --grad_accum_steps 1 \
   --early_stop "true" \
-  --patience 50 \
+  --patience 500 \
   \
   --wandb_project ${WANDB_PROJECT} \
-  --wandb_run "no_context_no_code_pretrain" \
+  --wandb_run "no_code_impute_finetune_lr${LR}" \
   \
-  --ckpt_dir ${PRETRAIN_CKPT} \
-  --pretrained_ckpt "none" \
-  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt"\
+  --ckpt_dir ${FINETUNE_CKPT} \
+  --pretrained_ckpt ${PRETRAIN_CKPT} \
+  --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
   \
   --generated_path "none" \
   \
   --gpu_id 0
+
+
 
 
 python dsp_flow.py \
