27c92b88bf596b7689547b48a7008aa8d321c772
Haochen Zhang <hz112@rice.edu>
2026-01-09 18:17:26 -0600

    0109

--- dsp_our_method/run_all_two_channels_mixed.sh
+++ dsp_our_method/run_all_two_channels_mixed.sh
@@ -1,6 +1,6 @@
 
 
 
-bash dsp_flow_mitdb_two_channels_mixed.sh &
-bash dsp_flow_qt_two_channels_mixed.sh &
-bash dsp_flow_svdb_two_channels_mixed.sh &
+CUDA_VISIBLE_DEVICES=3 bash dsp_flow_mitdb_two_channels_mixed.sh &
+CUDA_VISIBLE_DEVICES=4 bash dsp_flow_qt_two_channels_mixed.sh &
+CUDA_VISIBLE_DEVICES=5 bash dsp_flow_svdb_two_channels_mixed.sh &
