54656e29f0f60a2f148e705e32dd14405b2d1889
Haochen Zhang <hz112@rice.edu>
2026-01-10 10:59:09 -0600

    0110

--- install_packages.sh
+++ install_packages.sh
@@ -4,3 +4,4 @@ pip install peft==0.10.0
 pip install wandb
 pip install typing_extensions==4.15.0
 pip install catboost
+pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu118
