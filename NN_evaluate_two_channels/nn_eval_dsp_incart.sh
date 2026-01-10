e320e03e7d1e58f497ddcb648a61baeef33ecebf
Haochen Zhang <hz112@rice.edu>
2026-01-10 16:50:37 -0600

    0110

--- NN_evaluate_two_channels/nn_eval_dsp_incart.sh
+++ NN_evaluate_two_channels/nn_eval_dsp_incart.sh
@@ -9,8 +9,8 @@ NUM_CODES=$1
 
 DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_incart/I30.npz"]'
 FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I30npz/V_test.jsonl"]'
-FINETUNE_CKPT="../formal_experiment/incart/dsp_flow_mixed_K${NUM_CODES}/impute_finetune_ckpt_lr1e-4"
-OUTDIR="../nn_eval/incart/dsp_flow_mixed_K${NUM_CODES}"
+FINETUNE_CKPT="../formal_experiment/incart_6_channels/dsp_flow_mixed_K${NUM_CODES}/impute_finetune_ckpt_lr1e-4"
+OUTDIR="../nn_eval/incart_6_channels/dsp_flow_mixed_K${NUM_CODES}"
 
 MAX_LEN_ANOMALY=600
 LEN_WHOLE=800
--- NN_evaluate_two_channels/nn_eval_dsp_no_code_incart.sh
+++ NN_evaluate_two_channels/nn_eval_dsp_no_code_incart.sh
@@ -3,9 +3,9 @@ cd ..
 
 DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_incart/I30.npz"]'
 FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_incart/slide_windows_I30npz/V_test.jsonl"]'
-FINETUNE_CKPT="../formal_experiment/incart/dsp_flow_no_code/no_code_impute_finetune_ckpt_lr1e-4"
+FINETUNE_CKPT="../formal_experiment/incart_6_channels/dsp_flow_no_code/no_code_impute_finetune_ckpt_lr1e-4"
 
-OUTDIR="../nn_eval/incart/dspflow_no_code"
+OUTDIR="../nn_eval/incart_6_channels/dspflow_no_code"
 
 MAX_LEN_ANOMALY=600
 LEN_WHOLE=800
