e7d53941359e422a44c46bea6a826feebc6101c3
Haochen Zhang <hz112@rice.edu>
2026-01-12 13:48:03 -0600

    0112

--- diffusionts_baseline/diffts_mit106.sh
+++ diffusionts_baseline/diffts_mit106.sh
@@ -64,44 +64,44 @@ EVENT_LABELS_PATHS='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/
 #  --gpu_id 0
 #
 #
-python diffusion_ts.py \
-  --what_to_do "impute_sample" \
-  \
-  --seq_len ${LEN_WHOLE} \
-  --data_type ${DATA_TYPE} \
-  --feature_size ${FEAT_SIZE} \
-  --one_channel ${ONE_CHANNEL} \
-  \
-  --n_layer_enc 4 \
-  --n_layer_dec 4 \
-  --d_model 64 \
-  --n_heads 4 \
-  \
-  --raw_data_paths_train ${DATA_PATHS} \
-  --raw_data_paths_test ${TEST_DATA_PATHS} \
-  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
-  --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
-  --indices_paths_test "[]" \
-  --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
-  --min_infill_length ${MIN_LEN_ANOMALY} \
-  --max_infill_length ${MAX_LEN_ANOMALY} \
-  \
-  --lr 1e-4 \
-  --batch_size 128 \
-  --max_epochs 2000 \
-  --grad_clip_norm 1.0 \
-  --grad_accum_steps 1 \
-  --early_stop "true" \
-  --patience 50 \
-  \
-  --wandb_project "none" \
-  --wandb_run "none" \
-  \
-  --ckpt_dir ${TRAIN_CKPT} \
-  \
-  --generated_path "" \
-  \
-  --gpu_id 0
+#python diffusion_ts.py \
+#  --what_to_do "impute_sample" \
+#  \
+#  --seq_len ${LEN_WHOLE} \
+#  --data_type ${DATA_TYPE} \
+#  --feature_size ${FEAT_SIZE} \
+#  --one_channel ${ONE_CHANNEL} \
+#  \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  \
+#  --raw_data_paths_train ${DATA_PATHS} \
+#  --raw_data_paths_test ${TEST_DATA_PATHS} \
+#  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+#  --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
+#  --indices_paths_test "[]" \
+#  --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
+#  --min_infill_length ${MIN_LEN_ANOMALY} \
+#  --max_infill_length ${MAX_LEN_ANOMALY} \
+#  \
+#  --lr 1e-4 \
+#  --batch_size 128 \
+#  --max_epochs 2000 \
+#  --grad_clip_norm 1.0 \
+#  --grad_accum_steps 1 \
+#  --early_stop "true" \
+#  --patience 50 \
+#  \
+#  --wandb_project "none" \
+#  --wandb_run "none" \
+#  \
+#  --ckpt_dir ${TRAIN_CKPT} \
+#  \
+#  --generated_path "" \
+#  \
+#  --gpu_id 0
 
 
 #python diffusion_ts.py \
