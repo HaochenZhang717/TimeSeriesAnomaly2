22ccec46f53952af38f70682695e3a33c49bd3b7
Haochen Zhang <hz112@rice.edu>
2026-01-11 22:02:17 -0600

    0111

--- timevae_all/timevae_traffic.sh
+++ timevae_all/timevae_traffic.sh
@@ -53,7 +53,7 @@ python timevae_pipeline.py \
   \
   --lr ${LR} \
   --batch_size 64 \
-  --max_epochs 2000 \
+  --max_epochs 20 \
   --grad_clip_norm 1.0 \
   --grad_accum_steps 1 \
   --early_stop "true" \
