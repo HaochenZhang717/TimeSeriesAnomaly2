820162f665393204ae0b5c3af978c27276057985
Haochen Zhang <hz112@rice.edu>
2026-01-15 12:35:51 -0600

    0115

---
+++ GenIAS_all/genias_svdb.sh
@@ -0,0 +1,236 @@
+cd ..
+
+LR=1e-4
+LEN_WHOLE=800
+MAX_LEN_ANOMALY=450
+MIN_LEN_ANOMALY=80
+
+ONE_CHANNEL=0
+FEAT_SIZE=2
+
+DATA_TYPE="ecg"
+WANDB_PROJECT="GenIAS_svdb_two_channels"
+
+FINETUNE_CKPT="../formal_experiment/svdb_two_channels/GENIAS/ckpt_lr${LR}"
+
+
+DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_svdb/859.npz"]'
+TEST_DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_svdb/859.npz"]'
+FINETUNE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/V_train.jsonl"]'
+FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/V_test.jsonl"]'
+ANOMALY_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/V_segments_train.jsonl"]'
+NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/normal_800.jsonl"]'
+EVENT_LABELS_PATHS='["./dataset_utils/ECG_datasets/indices_svdb/slide_windows_859npz/event_label.npy"]'
+
+
+
+HIDDEN_LAYER_SIZES="[50,100,200]"
+TREND_POLY=3
+CUSTOM_SEAS="[[10,80],[20,40],[40,20],[80,10]]"
+LATENT_DIM=64
+
+DELTA_MIN=0.01
+DELTA_MAX=0.02
+PERTURB_WT=0.1
+KL_WT=1e-4
+SIGMA_PRIOR=0.5
+
+python genias_pipeline.py \
+  --what_to_do "genias_trian" \
+  \
+  --seq_len ${LEN_WHOLE} \
+  --data_type ${DATA_TYPE} \
+  --feature_size ${FEAT_SIZE} \
+  --one_channel ${ONE_CHANNEL} \
+  \
+  --hidden_layer_sizes ${HIDDEN_LAYER_SIZES} \
+  --trend_poly ${TREND_POLY} \
+  --custom_seas ${CUSTOM_SEAS} \
+  --delta_min ${DELTA_MIN} \
+  --delta_max ${DELTA_MAX} \
+  --perturbation_weight ${PERTURB_WT} \
+  --latent_dim ${LATENT_DIM}  \
+  --kl_wt ${KL_WT} \
+  --sigma_prior ${SIGMA_PRIOR} \
+  \
+  --raw_data_paths_train ${DATA_PATHS} \
+  --raw_data_paths_test ${TEST_DATA_PATHS} \
+  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+  --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
+  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+  --indices_paths_anomaly_for_sample "[]" \
+  --min_infill_length ${MIN_LEN_ANOMALY} \
+  --max_infill_length ${MAX_LEN_ANOMALY} \
+  \
+  --lr ${LR} \
+  --batch_size 64 \
+  --max_epochs 200 \
+  --grad_clip_norm 1.0 \
+  --grad_accum_steps 1 \
+  --early_stop "true" \
+  --patience 500 \
+  \
+  --wandb_project ${WANDB_PROJECT} \
+  --wandb_run "no_code_impute_lr${LR}" \
+  \
+  --ckpt_dir ${FINETUNE_CKPT} \
+  \
+  --generated_path "none" \
+  \
+  --gpu_id 0
+
+
+
+python genias_pipeline.py \
+  --what_to_do "principle_impute_sample" \
+  \
+  --seq_len ${LEN_WHOLE} \
+  --data_type ${DATA_TYPE} \
+  --feature_size ${FEAT_SIZE} \
+  --one_channel ${ONE_CHANNEL} \
+  \
+  --hidden_layer_sizes ${HIDDEN_LAYER_SIZES} \
+  --trend_poly ${TREND_POLY} \
+  --custom_seas ${CUSTOM_SEAS} \
+  --delta_min ${DELTA_MIN} \
+  --delta_max ${DELTA_MAX} \
+  --perturbation_weight ${PERTURB_WT} \
+  --latent_dim ${LATENT_DIM}  \
+  --kl_wt ${KL_WT} \
+  --sigma_prior ${SIGMA_PRIOR} \
+  \
+  --raw_data_paths_train ${DATA_PATHS} \
+  --raw_data_paths_test ${TEST_DATA_PATHS} \
+  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+  --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
+  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+  --indices_paths_anomaly_for_sample "[]" \
+  --min_infill_length ${MIN_LEN_ANOMALY} \
+  --max_infill_length ${MAX_LEN_ANOMALY} \
+  \
+  --lr ${LR} \
+  --batch_size 64 \
+  --max_epochs 200 \
+  --grad_clip_norm 1.0 \
+  --grad_accum_steps 1 \
+  --early_stop "true" \
+  --patience 500 \
+  \
+  --wandb_project ${WANDB_PROJECT} \
+  --wandb_run "no_code_impute_lr${LR}" \
+  \
+  --ckpt_dir ${FINETUNE_CKPT} \
+  \
+  --generated_path "none" \
+  \
+  --gpu_id 0
+
+
+python genias_pipeline.py \
+  --what_to_do "impute_sample_non_downstream" \
+  \
+  --seq_len ${LEN_WHOLE} \
+  --data_type ${DATA_TYPE} \
+  --feature_size ${FEAT_SIZE} \
+  --one_channel ${ONE_CHANNEL} \
+  \
+  --hidden_layer_sizes ${HIDDEN_LAYER_SIZES} \
+  --trend_poly ${TREND_POLY} \
+  --custom_seas ${CUSTOM_SEAS} \
+  --delta_min ${DELTA_MIN} \
+  --delta_max ${DELTA_MAX} \
+  --perturbation_weight ${PERTURB_WT} \
+  --latent_dim ${LATENT_DIM}  \
+  --kl_wt ${KL_WT} \
+  --sigma_prior ${SIGMA_PRIOR} \
+  \
+  --raw_data_paths_train ${DATA_PATHS} \
+  --raw_data_paths_test ${TEST_DATA_PATHS} \
+  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+  --indices_paths_train ${FINETUNE_TEST_INDICES_PATHS} \
+  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+  --indices_paths_anomaly_for_sample "[]" \
+  --min_infill_length ${MIN_LEN_ANOMALY} \
+  --max_infill_length ${MAX_LEN_ANOMALY} \
+  \
+  --lr ${LR} \
+  --batch_size 64 \
+  --max_epochs 200 \
+  --grad_clip_norm 1.0 \
+  --grad_accum_steps 1 \
+  --early_stop "true" \
+  --patience 500 \
+  \
+  --wandb_project ${WANDB_PROJECT} \
+  --wandb_run "no_code_impute_lr${LR}" \
+  \
+  --ckpt_dir ${FINETUNE_CKPT} \
+  \
+  --generated_path "none" \
+  \
+  --gpu_id 0
+
+
+#python genias_pipeline.py \
+#  --what_to_do "impute_normal_sample" \
+#  \
+#  --seq_len ${LEN_WHOLE} \
+#  --data_type ${DATA_TYPE} \
+#  --feature_size ${FEAT_SIZE} \
+#  --one_channel ${ONE_CHANNEL} \
+#  \
+#  --hidden_layer_sizes ${HIDDEN_LAYER_SIZES} \
+#  --trend_poly ${TREND_POLY} \
+#  --custom_seas ${CUSTOM_SEAS} \
+#  --delta_min ${DELTA_MIN} \
+#  --delta_max ${DELTA_MAX} \
+#  --perturbation_weight ${PERTURB_WT} \
+#  --latent_dim ${LATENT_DIM}  \
+#  --kl_wt ${KL_WT} \
+#  --sigma_prior ${SIGMA_PRIOR} \
+#  \
+#  --raw_data_paths_train ${DATA_PATHS} \
+#  --raw_data_paths_test ${TEST_DATA_PATHS} \
+#  --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+#  --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
+#  --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+#  --indices_paths_anomaly_for_sample "[]" \
+#  --min_infill_length ${MIN_LEN_ANOMALY} \
+#  --max_infill_length ${MAX_LEN_ANOMALY} \
+#  \
+#  --lr ${LR} \
+#  --batch_size 64 \
+#  --max_epochs 200 \
+#  --grad_clip_norm 1.0 \
+#  --grad_accum_steps 1 \
+#  --early_stop "true" \
+#  --patience 500 \
+#  \
+#  --wandb_project ${WANDB_PROJECT} \
+#  --wandb_run "no_code_impute_lr${LR}" \
+#  \
+#  --ckpt_dir ${FINETUNE_CKPT} \
+#  \
+#  --generated_path "none" \
+#  \
+#  --gpu_id 0
+
+
+OUTDIR="../nn_eval/svdb_two_channels/GENIAS"
+
+
+python run_nn_evaluate.py \
+    --seq_len ${LEN_WHOLE} \
+    --feature_size 2 \
+    --one_channel 0 \
+    --feat_window_size 300 \
+    --raw_data_paths ${DATA_PATHS} \
+    --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    --ckpt_dir "${FINETUNE_CKPT}" \
+    --out_dir "${OUTDIR}" \
+    --generated_path "${FINETUNE_CKPT}/principle_no_code_impute_samples.pth" \
+    --gpu_id 0
+
+cd GenIAS_all
+
