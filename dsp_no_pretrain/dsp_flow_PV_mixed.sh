dfc31dd45c72b579103707236c7cbd8d0a26f9d9
Haochen Zhang <hz112@rice.edu>
2026-01-18 14:33:01 -0600

    0118

---
+++ dsp_no_pretrain/dsp_flow_PV_mixed.sh
@@ -0,0 +1,208 @@
+cd ..
+
+export hucfg_t_sampling=logitnorm
+LR=1e-4
+LEN_WHOLE=200
+MAX_LEN_ANOMALY=144
+MIN_LEN_ANOMALY=20
+
+ONE_CHANNEL=1
+FEAT_SIZE=1
+
+DATA_TYPE="ecg"
+WANDB_PROJECT="dsp_flow_PV"
+
+
+DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_PV/2013_pv_sub_2.npz","./dataset_utils/ECG_datasets/raw_data_PV/2013_pv_sub_3.npz","./dataset_utils/ECG_datasets/raw_data_PV/2013_pv_sub_4.npz","./dataset_utils/ECG_datasets/raw_data_PV/2015_pv_sub_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2015_pv_sub_1.npz","./dataset_utils/ECG_datasets/raw_data_PV/2021_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2022_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2023_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2024_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2025_pv_live_0.npz"]'
+
+TEST_DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data_PV/2013_pv_sub_2.npz","./dataset_utils/ECG_datasets/raw_data_PV/2013_pv_sub_3.npz","./dataset_utils/ECG_datasets/raw_data_PV/2013_pv_sub_4.npz","./dataset_utils/ECG_datasets/raw_data_PV/2015_pv_sub_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2015_pv_sub_1.npz","./dataset_utils/ECG_datasets/raw_data_PV/2021_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2022_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2023_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2024_pv_live_0.npz","./dataset_utils/ECG_datasets/raw_data_PV/2025_pv_live_0.npz"]'
+
+PRETRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_2npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_3npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_4npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_1npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2021_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2022_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2023_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2024_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2025_pv_live_0npz/mixed.jsonl"]'
+
+FINETUNE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_2npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_3npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_4npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_0npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_1npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2021_pv_live_0npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2022_pv_live_0npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2023_pv_live_0npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2024_pv_live_0npz/V_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2025_pv_live_0npz/V_train.jsonl"]'
+
+FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_2npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_3npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_4npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_0npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_1npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2021_pv_live_0npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2022_pv_live_0npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2023_pv_live_0npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2024_pv_live_0npz/V_test.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2025_pv_live_0npz/V_test.jsonl"]'
+
+ANOMALY_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_2npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_3npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_4npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_0npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_1npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2021_pv_live_0npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2022_pv_live_0npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2023_pv_live_0npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2024_pv_live_0npz/V_segments_train.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2025_pv_live_0npz/V_segments_train.jsonl"]'
+
+NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_2npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_3npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_4npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_0npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_1npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2021_pv_live_0npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2022_pv_live_0npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2023_pv_live_0npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2024_pv_live_0npz/normal_200.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2025_pv_live_0npz/normal_200.jsonl"]'
+
+EVENT_LABELS_PATHS='["./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_2npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_3npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_4npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_0npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_1npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2021_pv_live_0npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2022_pv_live_0npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2023_pv_live_0npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2024_pv_live_0npz/event_label.npy","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2025_pv_live_0npz/event_label.npy"]'
+
+VQVAE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_2npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_3npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2013_pv_sub_4npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2015_pv_sub_1npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2021_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2022_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2023_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2024_pv_live_0npz/mixed.jsonl","./dataset_utils/ECG_datasets/indices_PV/slide_windows_2025_pv_live_0npz/mixed.jsonl"]'
+CODE_DIM=8
+CODE_LEN=4
+
+GPU_IDS=(0)
+NUM_CODES_LIST=(500)
+
+
+for i in ${!GPU_IDS[@]}; do
+
+  set -e
+
+  GPU=${GPU_IDS[$i]}
+  NUM_CODES=${NUM_CODES_LIST[$i]}
+  export CUDA_VISIBLE_DEVICES=${GPU}
+
+  VQVAE_CKPT="../formal_experiment/PV/dsp_flow_mixed_K${NUM_CODES}_no_pretrain/vqvae_save_path"
+  PRETRAIN_CKPT="../formal_experiment/PV/dsp_flow_mixed_K${NUM_CODES}_no_pretrain/no_context_pretrain_ckpt"
+  FINETUNE_CKPT="../formal_experiment/PV/dsp_flow_mixed_K${NUM_CODES}_no_pretrain/impute_finetune_ckpt_lr${LR}"
+
+  echo "Launching NUM_CODES=${NUM_CODES} on GPU ${GPU}"
+
+  python mini_runnable_vqvae.py \
+    --wandb_project ${WANDB_PROJECT} \
+    --wandb_run "VQVAE_mixed_-K${NUM_CODES}" \
+    --max_seq_len ${MAX_LEN_ANOMALY} \
+    --min_seq_len ${MIN_LEN_ANOMALY} \
+    --data_paths ${DATA_PATHS} \
+    --indices_paths ${VQVAE_TRAIN_INDICES_PATHS} \
+    --data_type ${DATA_TYPE} \
+    --gpu_id 0 \
+    --save_dir ${VQVAE_CKPT} \
+    --code_dim ${CODE_DIM} \
+    --code_len ${CODE_LEN} \
+    --num_codes ${NUM_CODES} \
+    --one_channel ${ONE_CHANNEL} \
+    --feat_size ${FEAT_SIZE}
+
+  python dsp_flow.py \
+    --what_to_do "no_code_imputation_from_scratch" \
+    --num_codes ${NUM_CODES} \
+    --seq_len ${LEN_WHOLE} \
+    --data_type ${DATA_TYPE} \
+    --feature_size ${FEAT_SIZE} \
+    --one_channel ${ONE_CHANNEL} \
+    --n_layer_enc 4 \
+    --n_layer_dec 4 \
+    --d_model 64 \
+    --n_heads 4 \
+    --raw_data_paths_train ${DATA_PATHS} \
+    --raw_data_paths_test ${TEST_DATA_PATHS} \
+    --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+    --indices_paths_train ${FINETUNE_TRAIN_INDICES_PATHS} \
+    --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+    --indices_paths_anomaly_for_sample "[]" \
+    --min_infill_length ${MIN_LEN_ANOMALY} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    --lr ${LR} \
+    --batch_size 64 \
+    --max_epochs 500 \
+    --grad_clip_norm 1.0 \
+    --grad_accum_steps 1 \
+    --early_stop "true" \
+    --patience 500 \
+    --wandb_project ${WANDB_PROJECT} \
+    --wandb_run "impute_finetune_mixed_lr${LR}_K${NUM_CODES}" \
+    --ckpt_dir ${FINETUNE_CKPT} \
+    --pretrained_ckpt ${PRETRAIN_CKPT} \
+    --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+    --generated_path "none" \
+    --gpu_id 0
+
+  python dsp_flow.py \
+    --what_to_do "principle_posterior_impute_sample" \
+    \
+    --num_codes ${NUM_CODES} \
+    --seq_len ${LEN_WHOLE} \
+    --data_type ${DATA_TYPE} \
+    --feature_size ${FEAT_SIZE} \
+    --one_channel ${ONE_CHANNEL} \
+    \
+    --n_layer_enc 4 \
+    --n_layer_dec 4 \
+    --d_model 64 \
+    --n_heads 4 \
+    \
+    --raw_data_paths_train ${DATA_PATHS} \
+    --raw_data_paths_test ${TEST_DATA_PATHS} \
+    --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+    --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
+    --indices_paths_test "[]" \
+    --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
+    --min_infill_length ${MIN_LEN_ANOMALY} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    \
+    --lr 1e-4 \
+    --batch_size 64 \
+    --max_epochs 2000 \
+    --grad_clip_norm 1.0 \
+    --grad_accum_steps 1 \
+    --early_stop "true" \
+    --patience 50 \
+    \
+    --wandb_project "none" \
+    --wandb_run "none" \
+    \
+    --ckpt_dir "${FINETUNE_CKPT}" \
+    --pretrained_ckpt "none" \
+    --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+    \
+    --generated_path "" \
+    \
+    --gpu_id 0
+
+  python dsp_flow.py \
+    --what_to_do "posterior_impute_sample_non_downstream" \
+    \
+    --num_codes ${NUM_CODES} \
+    --seq_len ${LEN_WHOLE} \
+    --data_type ${DATA_TYPE} \
+    --feature_size ${FEAT_SIZE} \
+    --one_channel ${ONE_CHANNEL} \
+    \
+    --n_layer_enc 4 \
+    --n_layer_dec 4 \
+    --d_model 64 \
+    --n_heads 4 \
+    \
+    --raw_data_paths_train ${DATA_PATHS} \
+    --raw_data_paths_test ${TEST_DATA_PATHS} \
+    --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+    --indices_paths_train ${FINETUNE_TEST_INDICES_PATHS} \
+    --indices_paths_test "[]" \
+    --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
+    --min_infill_length ${MIN_LEN_ANOMALY} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    \
+    --lr 1e-4 \
+    --batch_size 64 \
+    --max_epochs 2000 \
+    --grad_clip_norm 1.0 \
+    --grad_accum_steps 1 \
+    --early_stop "true" \
+    --patience 50 \
+    \
+    --wandb_project "none" \
+    --wandb_run "none" \
+    \
+    --ckpt_dir ${FINETUNE_CKPT} \
+    --pretrained_ckpt "none" \
+    --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+    \
+    --generated_path "" \
+    \
+    --gpu_id 0
+
+  OUTDIR="../nn_eval/PV/dsp_flow_mixed_K${NUM_CODES}_no_pretrain"
+  python run_nn_evaluate_new.py \
+    --seq_len ${LEN_WHOLE} \
+    --feature_size 1 \
+    --one_channel 1 \
+    --feat_window_size 100 \
+    --raw_data_paths ${DATA_PATHS} \
+    --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    --ckpt_dir "${FINETUNE_CKPT}" \
+    --out_dir "${OUTDIR}" \
+    --generated_path "${FINETUNE_CKPT}/principle_posterior_impute_samples.pth" \
+    --gpu_id 0
+
+done
+
+wait
+echo "All jobs finished."
+
+
+cd dsp_no_pretrain
---
+++ dsp_no_pretrain/dsp_flow_mitdb.sh
@@ -0,0 +1,208 @@
+cd ..
+
+export hucfg_t_sampling=logitnorm
+LR=1e-4
+LEN_WHOLE=1000
+MAX_LEN_ANOMALY=800
+MIN_LEN_ANOMALY=180
+
+ONE_CHANNEL=0
+FEAT_SIZE=2
+
+DATA_TYPE="ecg"
+WANDB_PROJECT="dsp_flow_mitdb_two_channels"
+
+
+DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data/106.npz"]'
+TEST_DATA_PATHS='["./dataset_utils/ECG_datasets/raw_data/106.npz"]'
+PRETRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/mixed.jsonl"]'
+FINETUNE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V_train.jsonl"]'
+FINETUNE_TEST_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V_test.jsonl"]'
+ANOMALY_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/anomaly_segments_with_prototype_train.jsonl"]'
+NORMAL_INDICES_FOR_SAMPLE='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal_1000.jsonl"]'
+EVENT_LABELS_PATHS='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/event_label.npy"]'
+
+#VQVAE Train Parameters
+VQVAE_TRAIN_INDICES_PATHS='["./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/mixed.jsonl"]'
+CODE_DIM=8
+CODE_LEN=4
+
+
+GPU_IDS=(0)
+NUM_CODES_LIST=(500)
+
+
+for i in ${!GPU_IDS[@]}; do
+
+  set -e
+
+  GPU=${GPU_IDS[$i]}
+  NUM_CODES=${NUM_CODES_LIST[$i]}
+  export CUDA_VISIBLE_DEVICES=${GPU}
+
+  VQVAE_CKPT="../formal_experiment/mitdb_two_channels/dsp_flow_mixed_K${NUM_CODES}_no_pretrain/vqvae_save_path"
+  PRETRAIN_CKPT="../formal_experiment/mitdb_two_channels/dsp_flow_mixed_K${NUM_CODES}_no_pretrain/no_context_pretrain_ckpt"
+  FINETUNE_CKPT="../formal_experiment/mitdb_two_channels/dsp_flow_mixed_K${NUM_CODES}_no_pretrain/impute_finetune_ckpt_lr${LR}"
+
+  echo "Launching NUM_CODES=${NUM_CODES} on GPU ${GPU}"
+
+  python mini_runnable_vqvae.py \
+    --wandb_project ${WANDB_PROJECT} \
+    --wandb_run "VQVAE_mixed_-K${NUM_CODES}" \
+    --max_seq_len ${MAX_LEN_ANOMALY} \
+    --min_seq_len ${MIN_LEN_ANOMALY} \
+    --data_paths ${DATA_PATHS} \
+    --indices_paths ${VQVAE_TRAIN_INDICES_PATHS} \
+    --data_type ${DATA_TYPE} \
+    --gpu_id 0 \
+    --save_dir ${VQVAE_CKPT} \
+    --code_dim ${CODE_DIM} \
+    --code_len ${CODE_LEN} \
+    --num_codes ${NUM_CODES} \
+    --one_channel ${ONE_CHANNEL} \
+    --feat_size ${FEAT_SIZE}
+
+  python dsp_flow.py \
+    --what_to_do "no_code_imputation_from_scratch" \
+    --num_codes ${NUM_CODES} \
+    --seq_len ${LEN_WHOLE} \
+    --data_type ${DATA_TYPE} \
+    --feature_size ${FEAT_SIZE} \
+    --one_channel ${ONE_CHANNEL} \
+    --n_layer_enc 4 \
+    --n_layer_dec 4 \
+    --d_model 64 \
+    --n_heads 4 \
+    --raw_data_paths_train ${DATA_PATHS} \
+    --raw_data_paths_test ${TEST_DATA_PATHS} \
+    --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+    --indices_paths_train ${FINETUNE_TRAIN_INDICES_PATHS} \
+    --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+    --indices_paths_anomaly_for_sample "[]" \
+    --min_infill_length ${MIN_LEN_ANOMALY} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    --lr ${LR} \
+    --batch_size 64 \
+    --max_epochs 2000 \
+    --grad_clip_norm 1.0 \
+    --grad_accum_steps 1 \
+    --early_stop "true" \
+    --patience 500 \
+    --wandb_project ${WANDB_PROJECT} \
+    --wandb_run "impute_finetune_mixed_lr${LR}_K${NUM_CODES}" \
+    --ckpt_dir ${FINETUNE_CKPT} \
+    --pretrained_ckpt ${PRETRAIN_CKPT} \
+    --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+    --generated_path "none" \
+    --gpu_id 0
+
+  python dsp_flow.py \
+    --what_to_do "posterior_impute_sample" \
+    \
+    --num_codes ${NUM_CODES} \
+    --seq_len ${LEN_WHOLE} \
+    --data_type ${DATA_TYPE} \
+    --feature_size ${FEAT_SIZE} \
+    --one_channel ${ONE_CHANNEL} \
+    \
+    --n_layer_enc 4 \
+    --n_layer_dec 4 \
+    --d_model 64 \
+    --n_heads 4 \
+    \
+    --raw_data_paths_train ${DATA_PATHS} \
+    --raw_data_paths_test ${TEST_DATA_PATHS} \
+    --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+    --indices_paths_train ${NORMAL_INDICES_FOR_SAMPLE} \
+    --indices_paths_test "[]" \
+    --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
+    --min_infill_length ${MIN_LEN_ANOMALY} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    \
+    --lr 1e-4 \
+    --batch_size 64 \
+    --max_epochs 2000 \
+    --grad_clip_norm 1.0 \
+    --grad_accum_steps 1 \
+    --early_stop "true" \
+    --patience 50 \
+    \
+    --wandb_project "none" \
+    --wandb_run "none" \
+    \
+    --ckpt_dir "${FINETUNE_CKPT}" \
+    --pretrained_ckpt "none" \
+    --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+    \
+    --generated_path "" \
+    \
+    --gpu_id 0
+
+
+python dsp_flow.py \
+    --what_to_do "posterior_impute_sample_non_downstream" \
+    \
+    --num_codes ${NUM_CODES} \
+    --seq_len ${LEN_WHOLE} \
+    --data_type ${DATA_TYPE} \
+    --feature_size ${FEAT_SIZE} \
+    --one_channel ${ONE_CHANNEL} \
+    \
+    --n_layer_enc 4 \
+    --n_layer_dec 4 \
+    --d_model 64 \
+    --n_heads 4 \
+    \
+    --raw_data_paths_train ${DATA_PATHS} \
+    --raw_data_paths_test ${TEST_DATA_PATHS} \
+    --event_labels_paths_train ${EVENT_LABELS_PATHS} \
+    --indices_paths_train ${FINETUNE_TEST_INDICES_PATHS} \
+    --indices_paths_test "[]" \
+    --indices_paths_anomaly_for_sample ${ANOMALY_INDICES_FOR_SAMPLE} \
+    --min_infill_length ${MIN_LEN_ANOMALY} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    \
+    --lr 1e-4 \
+    --batch_size 64 \
+    --max_epochs 2000 \
+    --grad_clip_norm 1.0 \
+    --grad_accum_steps 1 \
+    --early_stop "true" \
+    --patience 50 \
+    \
+    --wandb_project "none" \
+    --wandb_run "none" \
+    \
+    --ckpt_dir "${FINETUNE_CKPT}" \
+    --pretrained_ckpt "none" \
+    --vqvae_ckpt "${VQVAE_CKPT}/vqvae.pt" \
+    \
+    --generated_path "" \
+    \
+    --gpu_id 0
+
+
+OUTDIR="../nn_eval/mitdb_two_channels/dsp_flow_mixed_K${NUM_CODES}_no_pretrain"
+
+echo "Running nn_eval: ${FIENTUNE_CKPT}"
+
+python run_nn_evaluate.py \
+    --seq_len ${LEN_WHOLE} \
+    --feature_size 2 \
+    --one_channel 0 \
+    --feat_window_size 300 \
+    --raw_data_paths ${DATA_PATHS} \
+    --indices_paths_test ${FINETUNE_TEST_INDICES_PATHS} \
+    --max_infill_length ${MAX_LEN_ANOMALY} \
+    --ckpt_dir "${FINETUNE_CKPT}" \
+    --out_dir "${OUTDIR}" \
+    --generated_path "${FINETUNE_CKPT}/posterior_impute_samples.pth" \
+    --gpu_id 0
+
+done
+
+wait
+echo "All jobs finished."
+
+
+cd dsp_no_pretrain
